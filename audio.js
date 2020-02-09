e1972843a1ab82291ac2b7c6db8ced7861084ca2
nhodo <nhodo@me.com>
2020-02-09 16:52:40 -0600

    changed the way the playAudio function and document.ready function interact so that music will actually play

--- audio.js
+++ audio.js
@@ -1,14 +1,15 @@
-// when the document is ready:
-$(document).ready(function(){
-  // and the audio button is clicked: 
-  // audio button will dissappear and music will start:
+// when the audio button is clicked, 
+// the audio button will dissappear and music will start:
   function playAudio() { 
+    console.log("playAudio")
     var x = document.getElementById("myAudio");
     $("#disappear").css("display", "none")
     x.play(); 
   }
-  // after 15000 ms, a footer with nav link will show in case user
-  // wants to skip music and intro
+
+// after 15000 ms, a footer with nav link will show in case user
+// wants to skip music and intro
+  $(document).ready(function(){
   setTimeout(function(){
     document.getElementById('footerHide').classList.remove('hide');
   }, 10000);
