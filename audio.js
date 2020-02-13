40edaf111194a10f7fd58d00c34ae7930366b4cd
Neverender <tldavis09@yahoo.com>
2020-02-12 22:32:20 -0600

    styled the index page buttons

--- audio.js
+++ audio.js
@@ -17,19 +17,4 @@ function playAudio() {
 	x.onpause = function() {
 		isPlaying = false;
 	};
-	// $("#disappear").css("display", "none")
-	// x.play();
 }
-
-// after 15000 ms, a footer with nav link will show in case user
-// wants to skip music and intro
-$(document).ready(function() {
-	setTimeout(function() {
-		document.getElementById("footerHide").classList.remove("hide");
-	}, 10000);
-	// the ugly play music button [required by google :()]
-	// will disaaper after 1000 ms
-	setTimeout(function() {
-		document.getElementById("disappear").classList.add("hide");
-	}, 5000);
-});
