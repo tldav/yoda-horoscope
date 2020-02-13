a45638e204b6d4f62b63cb5497ec3b66e1a9b2b3
Neverender <tldavis09@yahoo.com>
2020-02-12 21:10:21 -0600

    let me know if you like this for the index. still working on it

--- audio.js
+++ audio.js
@@ -1,21 +1,35 @@
-// when the audio button is clicked, 
+// when the audio button is clicked,
 // the audio button will dissappear and music will start:
-  function playAudio() { 
-    console.log("playAudio")
-    var x = document.getElementById("myAudio");
-    $("#disappear").css("display", "none")
-    x.play(); 
-  }
+var isPlaying = false;
+
+function playAudio() {
+	console.log("playAudio");
+	var x = document.getElementById("myAudio");
+	if (isPlaying) {
+		x.pause();
+	} else {
+		x.play();
+	}
+
+	x.onplaying = function() {
+		isPlaying = true;
+	};
+	x.onpause = function() {
+		isPlaying = false;
+	};
+	// $("#disappear").css("display", "none")
+	// x.play();
+}
 
 // after 15000 ms, a footer with nav link will show in case user
 // wants to skip music and intro
-  $(document).ready(function(){
-  setTimeout(function(){
-    document.getElementById('footerHide').classList.remove('hide');
-  }, 10000);
-// the ugly play music button [required by google :()]
-// will disaaper after 1000 ms
-  setTimeout(function(){
-    document.getElementById('disappear').classList.add('hide');
-  }, 5000);
-})
+$(document).ready(function() {
+	setTimeout(function() {
+		document.getElementById("footerHide").classList.remove("hide");
+	}, 10000);
+	// the ugly play music button [required by google :()]
+	// will disaaper after 1000 ms
+	setTimeout(function() {
+		document.getElementById("disappear").classList.add("hide");
+	}, 5000);
+});
