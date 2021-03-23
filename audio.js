43717c832bca3c85b026702002e10418c6336826
Neverender <tldavis09@yahoo.com>
2021-03-22 21:37:10 -0500

    refactor if statement in audio to ternary operator

--- audio.js
+++ audio.js
@@ -2,11 +2,12 @@ let isPlaying = false;
 
 function playAudio() {
 	const audio = document.getElementById("audio");
-	if (isPlaying) {
-		audio.pause();
-	} else {
-		audio.play();
-	}
+	isPlaying ? audio.pause() : audio.play();
+	// if (isPlaying) {
+	// 	audio.pause();
+	// } else {
+	// 	audio.play();
+	// }
 
 	audio.onplaying = function () {
 		isPlaying = true;
