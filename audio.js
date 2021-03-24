a90b9712574461dd6be612db86921800c72591e5
Neverender <tldavis09@yahoo.com>
2021-03-23 21:04:25 -0500

    .

--- audio.js
+++ audio.js
@@ -3,11 +3,6 @@ let isPlaying = false;
 function playAudio() {
 	const audio = document.getElementById("audio");
 	isPlaying ? audio.pause() : audio.play();
-	// if (isPlaying) {
-	// 	audio.pause();
-	// } else {
-	// 	audio.play();
-	// }
 
 	audio.onplaying = function () {
 		isPlaying = true;
