85fae1bfc86e3a2036a9b05cad0475f88f68b8b4
Neverender <tldavis09@yahoo.com>
2021-04-04 22:21:53 -0500

    extract DOM variable from function

--- audio.js
+++ audio.js
@@ -1,7 +1,7 @@
+const audio = document.getElementById("audio");
 let isPlaying = false;
 
 function playAudio() {
-	const audio = document.getElementById("audio");
 	isPlaying ? audio.pause() : audio.play();
 
 	audio.onplaying = () => {
